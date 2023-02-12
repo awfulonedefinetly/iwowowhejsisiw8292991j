52cf48361c62b53b7caeae206f71c360e106b44b
awfulonedefinetly <124203718+awfulonedefinetly@users.noreply.github.com>
2023-02-12 23:25:55 +0100

    hsiwiwiwiwiwiwiwiwiw9w9283839wowk
    cv4fv4v4v5v4v4c4c4c4c5h6h5v5yb6un6by6h7yh6tvy try v5tv

--- heiwhsjwowowowpqp.py
+++ heiwhsjwowowowpqp.py
@@ -1,35 +1,75 @@
+# -- The bot was made by capitalist! --
+
+#pylint:disable=E0237
 from discord.ext import commands
 import discord
-import asyncpraw
+import praw
+import datetime
+import requests
+import asyncio
+from bs4 import BeautifulSoup
 
 
-intents = discord.Intents.default()
+intents = discord.Intents().all()
 intents.members = True
 intents.messages = True
 intents.message_content = True
 
-# Prefix will be '!'
-client = commands.Bot(command_prefix= '!', intents=intents)
+prefix = '!'
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-# Create an app on Reddit and get necessary information there
-reddit = asyncpraw.Reddit(
-client_id='',
-client_secret='',
-user_agent='Example u/reddit',
-username='user',
-password='admin'
+# Create an application on Reddit to get necessary data
+reddit = praw.Reddit(
+client_id='REDACTED',
+client_secret='REDACTED',
+user_agent='REDACTED',
+username='REDACTED',
+password='REDACTED'
 )
 
 reddit.read_only = False
 
-subreddit = reddit.subreddit('AgainstHateSubreddits')
 
-@client.event
-async def post_please():
-          while True:
-              for post in reddit.subreddit('AgainstHateSubreddits').new(limit=1000):
-                      channel = discord.utils.get(client.get_all_channels(), name='spam')
-                      await channel.send(post.title + post.url)
-              
-# Go to Discord Developer Portal                                
-client.run('Paste here Discord bot token')                                                                                         
+# Sends an alarm about new report            
+async def send_dm():
+            while True:
+                user_ids = "Paste your user id as a integer. Otherwise it will throw an exception."
+                subreddit = reddit.subreddit("memesofbased")
+                new_posts = subreddit.new(limit=1)
+                # Using a for loop to iterate over the posts
+                for x in new_posts:
+                    user = await bot.fetch_user(user_ids)
+                    await user.send(f"Your subreddit was reported in {subreddit.display_name}: {x.title}\n{x.url}")
+                # Checking new posts every 5 seconds
+                await asyncio.sleep(5)
+                
+                                                                                                                                      
+@bot.event
+async def on_ready():
+            now = datetime.datetime.now()
+            channel = discord.utils.get(bot.get_all_channels(), name='general')
+            embed = discord.Embed(title="The bot is now online!")
+            embed.add_field(name="Online: ", value=":green_circle:")
+            embed.add_field(name=f"The date is: ", value=str(now))
+            embed.add_field(name=":information_source:", value="Type !info for the commands!")
+            await channel.send(embed=embed)
+            bot.loop.create_task(send_dm())                                                                                                                                                                                                                           
+                                                                
+@bot.command()
+async def post(ctx):           
+           # Using same method as in send_dm()
+           subreddit = reddit.subreddit('AgainstHateSubreddits')
+            for post in subreddit.new(limit=1):
+                 new_embed = discord.Embed(title=post.title, url=post.shortlink)
+                 new_embed.add_field(name="Post text: ", value="Some text")
+                 new_embed.set_image(url=post.url) 
+                 await ctx.send(embed=new_embed)
+                      
+                      
+                           
+                                
+                                          
+if __name__ == "__main__":
+    # Go to Discord Developer Portal and create an application there
+    NEW_TOKEN = "DISCORD BOT TOKEN"
+    bot.run(NEW_TOKEN)                                                                                                                   
